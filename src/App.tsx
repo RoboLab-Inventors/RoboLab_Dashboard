f9fbe50414d4136c6828db37869d26c86ec47673
Ade <adepadova.ita@gmail.com>
2023-11-30 18:39:28 +0100

    Aggiustato il layout della scrollbar e della todolist

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,34 @@ import Header from "./components/Header";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import './style/App.css';
+import "./style/App.css";
 
 function App() {
   return (
     <>
       <Box className="appContainer">
-      <Header />
-      <Box className="gridContainer">
-        <Grid container spacing={2} className="gridBox" direction="row" sx={{ height: 'calc(100vh - 82.39px)' }}>
-          <Grid item xs={6}>
-            <Paper sx={{ height: 'calc(100vh - 97.39px)'}} className="paper">
-              <Todolist/>
-            </Paper>
+        <Header />
+        <Box className="gridContainer">
+          <Grid
+            container
+            spacing={2}
+            className="gridBox"
+            direction="row"
+            sx={{ height: "calc(100vh - 82.39px)" }}
+          >
+            <Grid item xs={6}>
+              <Paper sx={{ height: "calc(100vh - 97.39px)" }} className="scrollPadding">
+                <div className="paper"><Todolist /></div>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper sx={{ height: "calc(100vh - 97.39px)" }} className="paper">
+                prova
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Paper sx={{ height: 'calc(100vh - 97.39px)' }} className="paper">prova</Paper>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
-    </Box>
     </>
   );
 }
